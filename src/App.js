f21d3c1c4c21df80b415e2869aa38b6e2cebcd8d
maor-israel <77739201+maor-israel@users.noreply.github.com>
2022-03-22 16:45:03 +0200

    f

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
-import 'antd/dist/antd.css';
-import FirstScreen from './screens/js/firstScreen';
-
+import "./App.css";
+import "antd/dist/antd.css";
+import FirstScreen from "./screens/js/firstScreen";
 
 function App() {
   return (
     <div className="App">
-      <h1 id='logo'>Singit</h1>
+      <h1 id="logo">Singit</h1>
       <FirstScreen></FirstScreen>
     </div>
   );
